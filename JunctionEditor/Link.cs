7dcad448d30f2d354f1b21dcb4d66b533f14cdd9
endAurum <sqreder@gmail.com>
2014-01-26 23:59:44 +0400

    Implement basic variant of Link class

---
+++ JunctionEditor/Link.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainProject.JunctionEditor
+{
+    class Link: IDrawable
+    {
+        private Node from_, to_;
+        static Pen pen = new Pen(Color.DarkViolet, 2f)
+        {
+            DashCap = DashCap.Round,
+            DashPattern = new[] { 2.0F, 1.0F }
+        };
+
+        private Link(Node from, Node to)
+        {
+            from_ = from;
+            to_ = to;
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            Point a, b;
+            try
+            {
+                a = from_.GetPosition();
+                b = to_.GetPosition();
+            }
+
+            graphics.DrawLine(pen, a, b);
+        }
+    }
+}
