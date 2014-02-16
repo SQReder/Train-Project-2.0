cd724c0e0788636290f791b34dfbe2c7ce9bd28b
endAurum <sqreder@gmail.com>
2014-02-16 09:29:21 +0400

    Repair directory structures
    Move repo root up

---
+++ TrainProjectTests/JunctionRepositoryTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainProject.JunctionEditor;
+
+namespace TrainProjectTests
+{
+    [TestClass]
+    public class JunctionReposytoryTest
+    {
+        [TestMethod]
+        public void AddNodeTest()
+        {
+            var jr = new JunctionRepository();
+        }
+    }
+}
---
+++ TrainProjectTests/NodeTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainProject.JunctionEditor;
+
+namespace TrainProjectTests
+{
+    [TestClass]
+    public class NodeTest
+    {
+        [TestMethod]
+        public void EqualTest()
+        {
+            var actual = new Node
+            {
+                Type = Node.NodeType.Isolation,
+                Position = new PointF(0, 0),
+                Denominator = null,
+                Title = "actual"
+            };
+
+            var expected = new Node
+            {
+                Type = Node.NodeType.Isolation,
+                Position = new PointF(0, 0),
+                Denominator = null,
+                Title = "actual"
+            };
+
+            Assert.AreEqual<Node>(expected, actual);
+        }
+    }
+}
